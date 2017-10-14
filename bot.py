e3c4c494e2fe3294a71eddb6ca6e5ce30f6bcf76
Nikita Savostin <nikita@MacBook-Air-Nikita.local>
2017-10-14 15:06:44 +0300

    bot with webhooks1

--- bot.py
+++ bot.py
@@ -1,12 +1,34 @@
 import time
 import telebot
 import config
+from flask import Flask, request
+import os
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_msg(message):
-    bot.send_message(message.chat.id, message.text)
+server = Flask(__name__)
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/'+config.token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://chatbot0939.herokuapp.com/"+config.token)
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+ #   bot.set_webhook(url="https://chatbot0939.herokuapp.com/"+config.token)
+#if __name__ == '__main__':
+   # bot.polling(none_stop=True)
