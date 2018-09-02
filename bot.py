6a64211ed09688a0242c9061f80665a5f575a2f2
Nikita Savostin <nikita@MacBook-Air-Nikita.local>
2018-09-02 19:24:26 +0300

    add dialog alpha3

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(config.token)
 server = Flask(__name__)
 a=["Привет, смертный","Как дела, человечишка","Чем занимаешься", "Круто, хорошего дня"]
 b=["Привет, бот", "Хорошо", "Пишу код для тебя", "Пока"]
-
+i=0
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
@@ -36,7 +36,7 @@ def dialog(message):
     # Эти параметры для клавиатуры необязательны, просто для удобства
     global a
     global b
-    i=0
+    global i
     if(i<=len(a)):
 	    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 	    button_phone = types.KeyboardButton(text=a[i])
