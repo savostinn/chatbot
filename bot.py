51b8a2d217a3eb4e584eae9da649ea2c06f8ef04
Nikita Savostin <nikita@MacBook-Air-Nikita.local>
2018-09-02 19:12:41 +0300

    add dialog alpha

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import os
 bot = telebot.TeleBot(config.token)
 
 server = Flask(__name__)
+a=["Привет, смертный","Как дела, человечишка","Чем занимаешься", "Круто, хорошего дня"]
+b=["Привет, бот", "Хорошо", "Пишу код для тебя", "Пока"]
+i=0
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -29,14 +32,16 @@ def webhook():
     bot.set_webhook(url="https://chatbot0939.herokuapp.com/"+config.token)
     return "!", 200
 
-@bot.message_handler(commands=["geophone"])
-def geophone(message):
+@bot.message_handler(content_types=['text'])
+def dialog(message):
     # Эти параметры для клавиатуры необязательны, просто для удобства
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_phone = types.KeyboardButton(text="Отправить номер телефона")
-    button_geo = types.KeyboardButton(text="Отправить местоположение")
-    keyboard.add(button_phone, button_geo)
-    bot.send_message(message.chat.id, "Отправь мне свой номер телефона или поделись местоположением, жалкий человечишка!", reply_markup=keyboard)
+    global a
+    global b
+    if(i<=len(a)):
+	    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+	    button_phone = types.KeyboardButton(text=a[i])
+	    keyboard.add(button_phone)
+	    bot.send_message(message.chat.id, b[i], reply_markup=keyboard)
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 
