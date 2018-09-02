9d97d52da841ea289eb8555eaa00631565886e81
Nikita Savostin <nikita@MacBook-Air-Nikita.local>
2018-09-02 18:40:42 +0300

    add markups v3

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+#def echo_message(message):
+#    bot.reply_to(message, message.text)
 
 
 @server.route('/'+config.token, methods=['POST'])
@@ -33,7 +33,7 @@ def default_test(message):
     keyboard = types.ReplyKeyboardMarkup()
     url_button = types.KeyboardButton(text="Перейти на Яндекс")
     keyboard.add(url_button)
-    
+
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 
  #   bot.set_webhook(url="https://chatbot0939.herokuapp.com/"+config.token)
