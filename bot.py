e3a9d6bfabd5100fd945e13771a07644f23c1538
Nikita Savostin <nikita@MacBook-Air-Nikita.local>
2018-09-02 19:37:44 +0300

    add dialog alpha4

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ server = Flask(__name__)
 a=["Привет, смертный","Как дела, человечишка","Чем занимаешься", "Круто, хорошего дня"]
 b=["Привет, бот", "Хорошо", "Пишу код для тебя", "Пока"]
 i=0
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+#@bot.message_handler(commands=['start'])
+#def start(message):
+#    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 #@bot.message_handler(func=lambda message: True, content_types=['text'])
 #def echo_message(message):
@@ -31,17 +31,20 @@ def webhook():
     bot.set_webhook(url="https://chatbot0939.herokuapp.com/"+config.token)
     return "!", 200
 
-@bot.message_handler(content_types=['text'])
+#@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
 def dialog(message):
     # Эти параметры для клавиатуры необязательны, просто для удобства
     global a
     global b
     global i
-    if(i<=len(a)):
+    if i<=len(a):
 	    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-	    button_phone = types.KeyboardButton(text=a[i])
+	    button_phone = types.KeyboardButton(text=b[i])
 	    keyboard.add(button_phone)
-	    bot.send_message(message.chat.id, b[i], reply_markup=keyboard)
+	    bot.send_message(message.chat.id, a[i], reply_markup=keyboard)
+	    if i==len(a):
+	    	keyboard.removeKeyboard(True)
 	    i+=1
 
 
